a506d6ec73ca34d43558c677ca8314b9b715e55c
hirokihayami <hhdiddy.0426@gmail.com>
2021-06-19 23:37:48 +0900

    新規作成

---
+++ js/jquery2.js
@@ -0,0 +1,9 @@
+$(function() {
+$("#vegas").vegas({ // スライドショーにしたい要素指定
+    slides: [
+        { src: '../images/works1.jpg' }, // 背景画像1枚目
+        { src: '../images/works2.jpg' }, // 背景画像2枚目
+        { src: '../images/works3.jpg' } // 背景画像3枚目
+    ],
+  });
+});
